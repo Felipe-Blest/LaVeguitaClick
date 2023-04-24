c8a341a9939c272d3a0011e81ce782f7f4519ce9
duoc <git.duoc@duoc.cl>
2023-04-24 15:07:03 -0400

    Se agregó flecha para subir en página

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------
-    File Name: custom.js
+	File Name: custom.js
 ---------------------------------------------------------------------*/
 
 $(function () {
@@ -58,5 +58,14 @@ $(function () {
 		interval: 5000
 	});
 
-
+	/* Ir hacia arriba
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$(document).ready(function () { irArriba(); }); //Hacia arriba
+	function irArriba() {
+		$('.ir-arriba').click(function () { $('body,html').animate({ scrollTop: '0px' }, 10); });
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 0) { $('.ir-arriba').slideDown(600); } else { $('.ir-arriba').slideUp(600); }
+		});
+		$('.ir-abajo').click(function () { $('body,html').animate({ scrollTop: '1000px' }, 100); });
+	}
 });
